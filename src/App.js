91e1d799633c5bb0cb15e6874c8d9272751367f2
shahariarraghib <sr.raghib@gmail.com>
2022-09-13 22:44:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+// custom style for this application
+export const myTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#2D89FF',
+    },
+    secondary: {
+      main: '#642DFF',
+    },
+    paragraph1: {
+      main: '#575757',
+    },
+    textB: {
+      secondary: '#1E1E1E',
+    },
+   white: {
+      secondary: '#FFFFFF',
+    },
+  },
+
+});
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={myTheme}>
+   
+      <Home></Home>
+    
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from '../SharePages/NavigationBar';
+
+const Home = () => {
+    return (
+        <div>
+           
+            <NavigationBar></NavigationBar> 
+            
+        </div>
+    );
+};
+
+export default Home;
